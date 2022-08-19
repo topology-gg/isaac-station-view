7535d3323845ea781f0ac257758d0ce4af9c5e1b
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-08-19 18:49:54 +0100

    update univ and lobby addresses to open alpha

--- components/LobbyContract.js
+++ components/LobbyContract.js
@@ -3,7 +3,7 @@ import {
 } from '@starknet-react/core'
 
 import LobbyAbi from '../abi/lobby_abi.json'
-const LOBBY_ADDR = '0x05b26ace786dc2476c181f5bb3dce8b12073c57072c48b6037d7d67b2d749894'
+const LOBBY_ADDR = '0x06ea0fc5dcf98f2cb9f4b97fc355cd1f92e0ee83fde75c0f6117602a54cf6bda'
 
 export function useLobbyContract () {
     return useContract ({ abi: LobbyAbi, address: LOBBY_ADDR })
