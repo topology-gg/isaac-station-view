530f211c1f6f898514523bfba48b753a9a4def1c
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2022-08-29 21:46:27 +0200

    update contract address and abi

--- components/LobbyContract.js
+++ components/LobbyContract.js
@@ -3,7 +3,7 @@ import {
 } from '@starknet-react/core'
 
 import LobbyAbi from '../abi/lobby_abi.json'
-const LOBBY_ADDR = '0x01c9125178ed782ca3fd9bea67acf1d602376cf5203450946d9dd897bddc97e4'
+const LOBBY_ADDR = '0x03663ff3d4031bafa50581c00dc04aacb0103b2a54e7ee36390c2d767f452e33'
 
 export function useLobbyContract () {
     return useContract ({ abi: LobbyAbi, address: LOBBY_ADDR })
